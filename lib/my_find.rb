bfa894c2b44bf85383fec15cd1c999ad97c847d0
Charles <charlesangeloplai@gmail.com>
2020-11-14 01:01:43 -0500

    commit tree effb9527cfbf5d38669f09e6d32c4f734694ecbc

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
+    index = 0
 
+    while index < collection.length
+        return collection[index] if yield(collection[index])
+
+        index += 1
+    end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 } 
